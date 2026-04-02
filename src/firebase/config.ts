090473a1a61150a80f7793c45afbeff7d46dcf85
Akhila Tungachela <akhilatungachela@gmail.com>
2026-04-02 16:23:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9276032116-fd7af",
+  "appId": "1:951160414741:web:f13322b3d2c421e78aa41c",
+  "apiKey": "AIzaSyDhS4F2ANwi1P5TV3nJT3-_Cx1lBY13lYA",
+  "authDomain": "studio-9276032116-fd7af.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "951160414741"
+};
