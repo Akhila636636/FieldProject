c6127ba9651562e812ae5d86cace39c664a486c8
Akhila Tungachela <akhilatungachela@gmail.com>
2026-04-02 17:42:54 +0000

    still the same damn error, what are u even doing

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDhS4F2ANwi1P5TV3nJT3-_Cx1lBY13lYA",
-  authDomain: "studio-9276032116-fd7af.firebaseapp.com",
-  projectId: "studio-9276032116-fd7af",
-  storageBucket: "studio-9276032116-fd7af.appspot.com",
-  messagingSenderId: "951160414741",
-  appId: "1:951160414741:web:f13322b3d2c421e78aa41c"
-};
